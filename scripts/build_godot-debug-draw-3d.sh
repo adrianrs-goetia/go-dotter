6a606e448b60a10eaf68619cc6dbc03e62a5580c
adrian rs <adrian.r.strate@gmail.com>
2024-09-28 15:23:38 +0200

    (master) update godot_debug_draw_3d build script

--- scripts/build_godot-debug-draw-3d.sh
+++ scripts/build_godot-debug-draw-3d.sh
@@ -4,12 +4,19 @@ CWD="$(pwd)"
 
 ROOT="$(git rev-parse --show-toplevel)"
 cd $ROOT/godot_debug_draw_3d
-echo "DIR: $(pwd)"
 
+# Reset to origin of branch and then apply custom patches on top
+git reset --hard
 git apply --ignore-space-change --ignore-whitespace $ROOT/patches/godot_debug_draw_3d/godot-cpp.patch
 git apply --ignore-space-change --ignore-whitespace $ROOT/patches/godot_debug_draw_3d/sconstruct.patch
 git apply --ignore-space-change --ignore-whitespace $ROOT/patches/godot_debug_draw_3d/plugin-cpp-api.patch
 
 scons
+
+# godot_debug_draw_3d is assumed to be on level below go-dotter project root
+cd ..
+if [ ! -d project/addons/debug_draw_3d ]; then
+    ln -sr godot_debug_draw_3d/addons/debug_draw_3d/ project/addons/debug_draw_3d
+fi
 
 cd $CWD
