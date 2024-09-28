fbe0def4ba529bee2771154d6476211bc032a9b9
adrian rs <adrian.r.strate@gmail.com>
2024-09-28 19:17:44 +0200

    (master) update gameplay_cpp

--- gameplay_cpp/register_types.h
+++
@@ -1,9 +0,0 @@
-#ifndef GD_REGISTERTYPES_PLUGIN_GAMEPLAY_H
-#define GD_REGISTERTYPES_PLUGIN_GAMEPLAY_H
-
-#include <godot_cpp/core/class_db.hpp>
-
-void init_gameplay_plugin_module(godot::ModuleInitializationLevel p_level);
-void uninit_gameplay_plugin_module(godot::ModuleInitializationLevel p_level);
-
-#endif // GD_REGISTERTYPES_PLUGIN_GAMEPLAY_H
