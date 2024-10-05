e83d6e2d5b184ae0988b80907f601b1aff73f352
adrian rs <adrian.r.strate@gmail.com>
2024-10-05 20:47:06 +0200

    (versioning-export-and-release) Add versioning to gameplay_cpp.

--- gameplay_cpp/sconstruct.py
+++ gameplay_cpp/sconstruct.py
@@ -2,6 +2,7 @@ from SCons.Script.SConscript import SConsEnvironment
 from SCons.Variables import Variables, PathVariable
 import os
 import glob
+import re
 
 project_name = "gameplay_cpp"
 lib_name = "gameplay_cpp"
@@ -16,11 +17,45 @@ def get_object_files(env: SConsEnvironment):
         objs.append(env.Object(target=obj_target, source=src))
     return objs
 
+def get_library_version() -> str:
+    with open("gameplay_cpp/version.h", "r") as f:
+        header_content = f.read()
+
+    major_match = re.search(r"#define .*_MAJOR (\d+)", header_content)
+    minor_match = re.search(r"#define .*_MINOR (\d+)", header_content)
+    patch_match = re.search(r"#define .*_PATCH (\d+)", header_content)
+
+    major_value = int(major_match.group(1)) if major_match else 0
+    minor_value = int(minor_match.group(1)) if minor_match else 0
+    patch_value = int(patch_match.group(1)) if patch_match else 0
+
+    return f"{major_value}.{minor_value}.{patch_value}" 
+
+def get_libname_base() -> str:
+    return f"lib{lib_name}_v{get_library_version()}"
+
+def get_libname(env: SConsEnvironment, target: str) -> str:
+    return "{}_{}.{}.{}".format(get_libname_base(), env["platform"], target, env["arch"]) + env["SHLIBSUFFIX"]
+
+def generate_gdextension_file(env: SConsEnvironment):
+    with open(f"{current_dir}/templates/gdextension", "r") as template:
+        template_content = template.read()
+        with open(f"{current_dir}/addons/gameplay_cpp/gameplaycpp.gdextension", "w+") as gdextension:
+            gdextension.truncate(0)
+            template_buffer = template_content
+            template_buffer = re.sub(pattern="@libname_editor@", repl=f"{get_libname_base()}_", string=template_buffer)
+            template_buffer = re.sub(pattern="@libname_debug@", repl=f"{get_libname_base()}_", string=template_buffer)
+            template_buffer = re.sub(pattern="@libname_release@", repl=f"{get_libname_base()}_", string=template_buffer)
+            gdextension.write(template_buffer)
+            
+
 def configure_environment(env: SConsEnvironment, libs: list[str], args):
     e = env.Clone()
     e.Append(CPPPATH=os.path.join(current_dir, 'src'))
     e["use_hot_reload"] = True # hot reload by default for gameplay_cpp
-    lib_file = e.File("lib{}.{}.{}.{}".format(lib_name, env["platform"], env["target"], env["arch"]) + env["SHLIBSUFFIX"])
+
+    generate_gdextension_file(e)
+    lib_file = e.File(get_libname(e, env["target"]))
     
     e.SharedLibrary(
         target=lib_file,
