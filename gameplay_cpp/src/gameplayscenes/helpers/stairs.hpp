feb443deb1904ab0536e1988ac5cd46a1ab75861
adrian rs <adrian.r.strate@gmail.com>
2024-12-22 16:52:18 +0100

    (first-level) Stairs: Changing scene reallocates all children. Also check if node is ready before committing any logic, otherwise it screams and does almost nothing.

--- gameplay_cpp/src/gameplayscenes/helpers/stairs.hpp
+++ gameplay_cpp/src/gameplayscenes/helpers/stairs.hpp
@@ -9,35 +9,37 @@
 class GamesceneHelperStairs : public godot::Node3D {
 	GDCLASS(GamesceneHelperStairs, godot::Node3D)
 
+	G_PACKEDSCENE_IMPL(m_stairScene, StairScene)
 	G_INT_IMPL(m_num, Num, = 3)
 	G_FLOAT_IMPL(m_stepDepth, StepDepth, = -0.25f)
 	G_FLOAT_IMPL(m_stepHeight, StepHeight, = 0.25f)
 	G_FLOAT_IMPL(m_stepScale, StepScale, = 0.3f)
 	G_FLOAT_IMPL(m_curveAngle, CurveAngle) // angle in degrees
-	GS_PACKEDSCENE_IMPL(m_stairScene, StairScene)
 
 public:
 	static void _bind_methods() {
+		METHOD_PROPERTY_PACKEDSCENE_IMPL(GamesceneHelperStairs, StairScene)
 		METHOD_PROPERTY_IMPL(GamesceneHelperStairs, Num, INT)
 		METHOD_PROPERTY_IMPL(GamesceneHelperStairs, StepDepth, FLOAT)
 		METHOD_PROPERTY_IMPL(GamesceneHelperStairs, StepHeight, FLOAT)
 		METHOD_PROPERTY_IMPL(GamesceneHelperStairs, StepScale, FLOAT)
 		METHOD_PROPERTY_IMPL(GamesceneHelperStairs, CurveAngle, FLOAT)
-		METHOD_PROPERTY_PACKEDSCENE_IMPL(GamesceneHelperStairs, StairScene)
 	}
 
 	void _enter_tree() override {
 		set_meta("_edit_group_", true);
 		set_notify_transform(true);
 
-		setAmountOfStairs();
-		adjustStairTransform();
+		if (m_stairScene.is_valid()) {
+			allocateAmount();
+			adjustTransform();
+		}
 	}
 
 	void _notification(int what) {
 		switch (what) {
 			case NOTIFICATION_TRANSFORM_CHANGED:
-				adjustStairTransform();
+				adjustTransform();
 				break;
 
 			default:
@@ -45,8 +47,8 @@ public:
 		}
 	}
 
-	void setAmountOfStairs() {
-		if (!m_stairScene.is_valid()) {
+	void allocateAmount() {
+		if (m_stairScene.is_null()) {
 			LOG(ERROR, "Required scene not set for stairs")
 			return;
 		}
@@ -82,9 +84,33 @@ public:
 		}
 	}
 
-	void adjustStairTransform() {
+	void resetAll() {
+		removeAll();
+
+		ASSERT(m_stairScene.is_valid())
+		Node* step = nullptr;
+		step = m_stairScene->instantiate();
+		ASSERT_NOTNULL(step)
+		add_child(step);
+		for (int i = 1; i < m_num; i++) {
+			auto* nextStep = m_stairScene->instantiate();
+			ASSERT_NOTNULL(step)
+			step->add_sibling(nextStep);
+			step = nextStep;
+		}
+		adjustTransform();
+	}
+
+	void removeAll() {
+		auto children = get_children();
+		while (children.size()) {
+			auto* child = cast_to<Node>(children.pop_back());
+			remove_child(child);
+		}
+	}
+
+	void adjustTransform() {
 		if (get_child_count() == 0) {
-			LOG(DEBUG, "Tried adjusting stair positions without any child nodes")
 			return;
 		}
 
@@ -117,31 +143,48 @@ public:
 		}
 	}
 
+	void setStairScene(godot::Ref<godot::PackedScene> value) {
+		m_stairScene = value;
+
+		if (m_stairScene.is_null()) {
+			removeAll();
+			return;
+		}
+
+		if (is_node_ready())
+			resetAll();
+	}
+
 	void setNum(int value) {
 		m_num = std::max(value, 1);
-		if (m_num > 0) {
-			setAmountOfStairs();
-			adjustStairTransform();
+		if (m_num > 0 && is_node_ready()) {
+			allocateAmount();
+			adjustTransform();
 		}
 	}
 
 	void setStepDepth(float value) {
 		m_stepDepth = value;
-		adjustStairTransform();
+
+		if (is_node_ready())
+			adjustTransform();
 	}
 
 	void setStepHeight(float value) {
 		m_stepHeight = value;
-		adjustStairTransform();
+		if (is_node_ready())
+			adjustTransform();
 	}
 
 	void setStepScale(float value) {
 		m_stepScale = value;
-		adjustStairTransform();
+		if (is_node_ready())
+			adjustTransform();
 	}
 
 	void setCurveAngle(float value) {
 		m_curveAngle = value;
-		adjustStairTransform();
+		if (is_node_ready())
+			adjustTransform();
 	}
 };
