f9b34e94dfdce74a9a5177a307c9b6f5fbf49f7f
adrian rs <adrian.r.strate@gmail.com>
2024-12-31 13:59:01 +0100

    (custom-anim-node) Custom OneShot node which can change bone filtering.

---
+++ gameplay_cpp/src/character/animation/typedefs.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+namespace bones {
+
+constexpr const char* upperBody[] = {
+	"Armature/Skeleton3D:mixamorig_Hips",
+
+	"Armature/Skeleton3D:mixamorig_Spine",
+	"Armature/Skeleton3D:mixamorig_Spine1",
+	"Armature/Skeleton3D:mixamorig_Spine2",
+	"Armature/Skeleton3D:mixamorig_Neck",
+	"Armature/Skeleton3D:mixamorig_Head",
+
+	"Armature/Skeleton3D:mixamorig_LeftShoulder",
+	"Armature/Skeleton3D:mixamorig_LeftArm",
+	"Armature/Skeleton3D:mixamorig_LeftForeArm",
+	"Armature/Skeleton3D:mixamorig_LeftHand",
+	"Armature/Skeleton3D:mixamorig_LeftHandThumb1",
+	"Armature/Skeleton3D:mixamorig_LeftHandThumb2",
+	"Armature/Skeleton3D:mixamorig_LeftHandThumb3",
+	"Armature/Skeleton3D:mixamorig_LeftHandIndex1",
+	"Armature/Skeleton3D:mixamorig_LeftHandIndex2",
+	"Armature/Skeleton3D:mixamorig_LeftHandIndex3",
+	"Armature/Skeleton3D:mixamorig_LeftHandMiddle1",
+	"Armature/Skeleton3D:mixamorig_LeftHandMiddle2",
+	"Armature/Skeleton3D:mixamorig_LeftHandMiddle3",
+	"Armature/Skeleton3D:mixamorig_LeftHandRing1",
+	"Armature/Skeleton3D:mixamorig_LeftHandRing2",
+	"Armature/Skeleton3D:mixamorig_LeftHandRing3",
+	"Armature/Skeleton3D:mixamorig_LeftHandPinky1",
+	"Armature/Skeleton3D:mixamorig_LeftHandPinky2",
+	"Armature/Skeleton3D:mixamorig_LeftHandPinky3",
+
+	"Armature/Skeleton3D:mixamorig_RightShoulder",
+	"Armature/Skeleton3D:mixamorig_RightArm",
+	"Armature/Skeleton3D:mixamorig_RightForeArm",
+	"Armature/Skeleton3D:mixamorig_RightHand",
+	"Armature/Skeleton3D:mixamorig_RightHandThumb1",
+	"Armature/Skeleton3D:mixamorig_RightHandThumb2",
+	"Armature/Skeleton3D:mixamorig_RightHandThumb3",
+	"Armature/Skeleton3D:mixamorig_RightHandIndex1",
+	"Armature/Skeleton3D:mixamorig_RightHandIndex2",
+	"Armature/Skeleton3D:mixamorig_RightHandIndex3",
+	"Armature/Skeleton3D:mixamorig_RightHandMiddle1",
+	"Armature/Skeleton3D:mixamorig_RightHandMiddle2",
+	"Armature/Skeleton3D:mixamorig_RightHandMiddle3",
+	"Armature/Skeleton3D:mixamorig_RightHandRing1",
+	"Armature/Skeleton3D:mixamorig_RightHandRing2",
+	"Armature/Skeleton3D:mixamorig_RightHandRing3",
+	"Armature/Skeleton3D:mixamorig_RightHandPinky1",
+	"Armature/Skeleton3D:mixamorig_RightHandPinky2",
+	"Armature/Skeleton3D:mixamorig_RightHandPinky3",
+};
+
+constexpr const char* lowerBody[] = {
+	"Armature/Skeleton3D:mixamorig_LeftUpLeg",
+	"Armature/Skeleton3D:mixamorig_LeftLeg",
+	"Armature/Skeleton3D:mixamorig_LeftFoot",
+	"Armature/Skeleton3D:mixamorig_LeftToeBase",
+
+	"Armature/Skeleton3D:mixamorig_RightUpLeg",
+	"Armature/Skeleton3D:mixamorig_RightLeg",
+	"Armature/Skeleton3D:mixamorig_RightFoot",
+	"Armature/Skeleton3D:mixamorig_RightToeBase",
+};
+
+} //namespace bones
